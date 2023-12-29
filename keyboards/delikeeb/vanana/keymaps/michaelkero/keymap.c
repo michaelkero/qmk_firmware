d5ee04e1b6b94fe611ab00b6b8b6c7e9d2a90079
Michael Kero <michael.kero@gmail.com>
2023-12-29 12:19:22 -0500

    only left alt for some windows qwirk

--- keyboards/delikeeb/vanana/keymaps/michaelkero/keymap.c
+++ keyboards/delikeeb/vanana/keymaps/michaelkero/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB ,     KC_Q   ,    KC_W   ,    KC_E   ,    KC_R ,    KC_T          ,                 KC_Y          ,    KC_U   ,    KC_I   ,    KC_O   ,    KC_P   ,    KC_BSLS,
     KC_ESC ,     KC_A   ,    KC_S   ,    KC_D   ,    KC_F ,    KC_G          ,     KC_VOLU,    KC_H          ,    KC_J   ,    KC_K   ,    KC_L   ,    KC_SCLN,    KC_QUOT,
     KC_LSFT,     KC_Z   ,    KC_X   ,    KC_C   ,    KC_V ,    KC_B          ,     KC_VOLD,    KC_N          ,    KC_M   ,    KC_COMM,    KC_DOT ,    KC_SLSH,    RSFT_T(KC_DEL),
-                 KC_LCTL,    KC_LALT,    KC_LGUI,    LOWER,    LSFT_T(KC_SPC),                 RCTL_T(KC_ENT),    RAISE  ,    KC_RALT,    KC_HOME,    KC_END
+                 KC_LCTL,    KC_LALT,    KC_LGUI,    LOWER,    LSFT_T(KC_SPC),                 RCTL_T(KC_ENT),    RAISE  ,    KC_LALT,    KC_HOME,    KC_END
     ),
 
     [_LOWER] = LAYOUT(
