186b5c62e8cb22f59feb36b11d63efd4200cc074
Michael Kero <michael.kero@gmail.com>
2024-07-10 15:31:49 -0400

    added keymap for sofle

--- keyboards/sofle/keymaps/michaelkero/keymap.c
+++ keyboards/sofle/keymaps/michaelkero/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
     _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
     _______,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,_______,
-    _______,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
-    CTL_GRV,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_MUTE,_______,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,_______,
+    CTL_GRV,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
+    _______,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_MUTE,_______,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,_______,
                     _______,_______,KC_LGUI,LO_ESC ,SFT_SPC,CTL_ENT,RA_BSP ,KC_LALT,_______,_______
 ),
 
