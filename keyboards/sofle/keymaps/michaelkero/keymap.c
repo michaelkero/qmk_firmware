409ee65adafc445a23f65b4ba5a1f17536ddc8a8
Michael Kero <michael.kero@gmail.com>
2024-07-19 11:28:17 -0400

    tweaks to backspace and delete thumbs sofle

--- keyboards/sofle/keymaps/michaelkero/keymap.c
+++ keyboards/sofle/keymaps/michaelkero/keymap.c
@@ -5,6 +5,7 @@
 #define CTL_Z LCTL_T(KC_Z)
 #define SFT_SPC LSFT_T(KC_SPC)
 #define CTL_ENT LCTL_T(KC_ENT)
+#define SFT_SLH LSFT_T(KC_SLSH)
 
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
@@ -32,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
     _______,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,_______,
     _______,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,_______,
-    _______,CTL_Z  ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_MUTE,_______,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,_______,
+    _______,CTL_Z  ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_MUTE,_______,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,SFT_SLH,_______,
                     _______,_______,KC_LGUI,LO_ESC ,SFT_SPC,CTL_ENT,RA_BSP ,KC_LALT,_______,_______
 ),
 
@@ -41,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,KC_GRAVE,KC_TILD,KC_CIRC,KC_PAST,KC_PPLS,                KC_HASH,KC_LCBR,KC_RCBR,KC_LABK,KC_RABK,_______,
     _______,KC_PIPE ,KC_AMPR,KC_EXLM,KC_EQL ,KC_UNDS,                KC_DLR ,KC_LPRN,KC_RPRN,KC_QUOT,KC_DQT ,_______,
     _______,KC_BSLS ,_______,KC_PERC,KC_SLSH,KC_MINS,_______,_______,KC_AT  ,KC_LBRC,KC_RBRC,_______,_______,_______,
-                     _______,_______,_______,_______,_______,_______,KC_DEL ,_______,_______,_______
+                     _______,_______,_______,_______,_______,KC_DEL ,KC_BSPC,_______,_______,_______
 ),
 
 [_RAISE] = LAYOUT(
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,_______,
     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,_______,                KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-                    _______,_______,_______,_______,KC_TAB ,_______,_______,_______,_______,_______
+                    _______,_______,_______,KC_ESC ,KC_TAB ,_______,_______,_______,_______,_______
 ),
 
 [_ADJUST] = LAYOUT( 
