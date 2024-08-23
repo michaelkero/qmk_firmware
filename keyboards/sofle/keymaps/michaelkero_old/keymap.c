dd0d9b527979ef6c6d6fc02c1c063e0d6a769d0e
Michael Kero <michael.kero@gmail.com>
2024-08-23 15:47:10 -0400

    added my keymap and all changes to make this complie

--- keyboards/sofle/keymaps/michaelkero_old/keymap.c
+++ keyboards/sofle/keymaps/michaelkero_old/keymap.c
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
     _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
     _______,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,_______,
-    KC_ESC ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_BSPC,
-    KC_LCTL,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_MUTE,_______,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_LSFT,
+    _______,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,_______,
+    _______,CTL_Z  ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_MUTE,_______,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,SFT_SLH,_______,
                     _______,_______,KC_LGUI,LO_ESC ,SFT_SPC,CTL_ENT,RA_BSP ,KC_LALT,_______,_______
 ),
 
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,_______ ,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
     _______,KC_GRAVE,KC_TILD,KC_CIRC,KC_PAST,KC_PPLS,                KC_HASH,KC_LCBR,KC_RCBR,KC_LABK,KC_RABK,_______,
     _______,KC_PIPE ,KC_AMPR,KC_EXLM,KC_EQL ,KC_UNDS,                KC_DLR ,KC_LPRN,KC_RPRN,KC_QUOT,KC_DQT ,_______,
-    _______,_______ ,_______,KC_PERC,KC_SLSH,KC_MINS,_______,_______,KC_AT  ,KC_LBRC,KC_RBRC,_______,KC_BSLS,_______,
-                     _______,_______,_______,_______,_______,KC_DEL ,_______,_______,_______,_______
+    _______,KC_BSLS ,_______,KC_PERC,KC_SLSH,KC_MINS,_______,_______,KC_AT  ,KC_LBRC,KC_RBRC,_______,_______,_______,
+                     _______,_______,_______,_______,_______,KC_DEL ,KC_BSPC,_______,_______,_______
 ),
 
 [_RAISE] = LAYOUT(
@@ -298,4 +298,3 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 
 #endif
-
